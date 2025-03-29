c84df4d8a34221039b2688d12c3fe42ac81c619f
VitaleCoding <legosam1031@gmail.com>
2025-03-29 15:33:37 -0400

    Updated Blackjack.js and added Blackjack.css and cards.js

---
+++ src/cards.js
@@ -0,0 +1,22 @@
+// cards.js
+export const cardDeck = [
+  '♠A', '♠2', '♠3', '♠4', '♠5', '♠6', '♠7', '♠8', '♠9', '♠10', '♠J', '♠Q', '♠K', 
+  '♥A', '♥2', '♥3', '♥4', '♥5', '♥6', '♥7', '♥8', '♥9', '♥10', '♥J', '♥Q', '♥K', 
+  '♦A', '♦2', '♦3', '♦4', '♦5', '♦6', '♦7', '♦8', '♦9', '♦10', '♦J', '♦Q', '♦K', 
+  '♣A', '♣2', '♣3', '♣4', '♣5', '♣6', '♣7', '♣8', '♣9', '♣10', '♣J', '♣Q', '♣K'
+];
+
+// Function to shuffle the deck
+export function shuffleDeck(deck) {
+  let shuffledDeck = [...deck];
+  for (let i = shuffledDeck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+  }
+  return shuffledDeck;
+}
+
+// Function to deal a card
+export function dealCard(deck) {
+  return deck.pop();  // remove and return the last card in the deck
+}
